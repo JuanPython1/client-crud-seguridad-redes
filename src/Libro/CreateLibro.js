2adec2651d0134aae46652f7d791da79f04ceabf
JuanR <112212338+JuanPython1@users.noreply.github.com>
2024-02-22 00:34:54 -0500

    cambios URI

--- src/Libro/CreateLibro.js
+++ src/Libro/CreateLibro.js
@@ -2,8 +2,7 @@ import axios from 'axios'
 import {useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const URI = 'http://localhost:8000/libros/'
-
+const URI = 'crud-seguridad-redes-production.up.railway.app/libros/' 
 
 const CompCreateLibro = () => {
     const [Titulo, setTitulo] = useState('')
